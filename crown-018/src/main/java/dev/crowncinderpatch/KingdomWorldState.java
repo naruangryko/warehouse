92f1a8d123e24235b6e0449cc01914fd98012640
naruangryko <naruangry@gmail.com>
2026-09-12 19:50:05 +0900

    feat: track 0.21 capital upgrade state

--- crown-018/src/main/java/dev/crowncinderpatch/KingdomWorldState.java
+++ crown-018/src/main/java/dev/crowncinderpatch/KingdomWorldState.java
@@ -4,13 +4,14 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.PersistentState;
 
-/** Stores the immutable world anchor, surface-capital state, and automatic NPC population state. */
+/** Stores immutable kingdom anchor and one-time versioned capital upgrade state. */
 public final class KingdomWorldState extends PersistentState {
     public boolean anchorSet;
     public int anchorX;
     public int anchorZ;
     public boolean built020;
     public boolean npcsSeeded;
+    public boolean built021;
 
     public static KingdomWorldState get(ServerWorld world) {
         return world.getPersistentStateManager().getOrCreate(KingdomWorldState::read, KingdomWorldState::new, "crowncinder_020_world");
@@ -23,6 +24,7 @@ public final class KingdomWorldState extends PersistentState {
         s.anchorZ = nbt.getInt("anchorZ");
         s.built020 = nbt.getBoolean("built020");
         s.npcsSeeded = nbt.getBoolean("npcsSeeded");
+        s.built021 = nbt.getBoolean("built021");
         return s;
     }
 
@@ -33,6 +35,7 @@ public final class KingdomWorldState extends PersistentState {
         nbt.putInt("anchorZ", anchorZ);
         nbt.putBoolean("built020", built020);
         nbt.putBoolean("npcsSeeded", npcsSeeded);
+        nbt.putBoolean("built021", built021);
         return nbt;
     }
 }
