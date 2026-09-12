1db651b8804d5b51cfd1c84a2a78c932fe478c3f
naruangryko <naruangry@gmail.com>
2026-09-12 21:25:34 +0900

    fix: use Yarn 1.20.1 status effect package

--- crown-018/src/main/java/dev/crowncinderpatch/ArcaneStaffItem023.java
+++ crown-018/src/main/java/dev/crowncinderpatch/ArcaneStaffItem023.java
@@ -1,6 +1,8 @@
 package dev.crowncinderpatch;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -11,8 +13,6 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.status_effect.StatusEffectInstance;
-import net.minecraft.status_effect.StatusEffects;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
